e8fba5a52b6d6c9c6362a8dfe53a257a5921827f
Matthew Thompson <Matthew8.Thompson@live.uwe.ac.uk>
2018-10-31 20:10:44 +0000

    Started program

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+    Author: Matthew Thompson
+    App Name: SSPhoto
+    Description: Scans and sorts photos 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Main program start */
+int main(void) {
+
+
+
+}
